123e57b1c5ddf2725991fe1f3a8b5673b823595b
jsutanto <jsutanto@JSUTANTO-PC.ds.swin.edu.au>
2014-02-22 13:48:51 +1100

    Refactoring method

--- src/edu/swin/SwinNetSim/TestDownload.java
+++ src/edu/swin/SwinNetSim/TestDownload.java
@@ -13,31 +13,47 @@ public class TestDownload {
 	public final static String SMALL_FILE = "texter/small";
 	public final static String MEDIUM_FILE = "texter/medium";
 	public final static String LARGE_FILE = "texter/large";
-	public final static String XLARGE_FILE_FILE = "texter/xlarge";
+	public final static String XLARGE_FILE = "texter/xlarge";
 	
 	public final static int REPEAT = 100;
 	
-	public static void main(String [] args) throws IOException, JSONException {
-		// Get the starting time
-		StopWatch watch = new StopWatch();
-		
+	public static void main(String [] args) throws IOException, JSONException {		
 		System.out.println("=====================================================");
 		System.out.println("Running on server url " + SERVER_URL);
 		System.out.println("=====================================================");
 		
 		// SMALL_FILE over REPEAT times
 		System.out.println("Downloading small files...");
+		testDownload(TestDownload.SMALL_FILE);
+		
+		System.out.println("=====================================================");
+		System.out.println("Downloading medium files...");
+		testDownload(TestDownload.MEDIUM_FILE);
+		
+		System.out.println("=====================================================");
+		System.out.println("Downloading large files...");
+		testDownload(TestDownload.LARGE_FILE);
+		
+		System.out.println("=====================================================");
+		System.out.println("Downloading x-large files...");
+		testDownload(TestDownload.XLARGE_FILE);
+	}
 
+	private static void testDownload(String testFile) throws IOException,
+			JSONException {
+		// Get the starting time
+		StopWatch watch = new StopWatch();
 		watch.start();
 		for(int i = 0; i < TestDownload.REPEAT; i++) {
-			JSONObject json = JsonReader.readJsonFromUrl(SERVER_URL + SMALL_FILE);
+			JSONObject json = JsonReader.readJsonFromUrl(SERVER_URL + testFile);
 			System.out.print('.');
 		}
 		watch.stop();
+		long totalTime = watch.getTime();
 
 		System.out.println("done!");
-		System.out.println("Time taken: " + watch.getTime());
-		
+		System.out.println("Time taken: " + totalTime + " ms");
+		System.out.println("On average: " + (totalTime / TestDownload.REPEAT) + " ms");
 	}
 
 }
